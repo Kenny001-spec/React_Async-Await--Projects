444af2a8c68e4198ce921ac6c8d5d11416d4a739
Abubakar kehinde <abubakarkehinde88@gmail.com>
2023-12-12 15:43:08 +0100

    update

--- src/components/Jokes.jsx
+++ src/components/Jokes.jsx
@@ -1,30 +1,30 @@
-import React from 'react'
-import useAxios from '../hooks/useAxios'
-import axios from '../apis/dadjokes'
+import React from "react";
+import useAxios from "../hooks/useAxios";
+import axios from "../apis/dadjokes";
 
 const Jokes = () => {
-    const [joke, loading, error, refetch] = useAxios({
-        axiosInstance : axios,
-        url : '/',
-        method : 'GET',
-        requestConfig : {
-            headers: {
-                'Content-Language' : 'en-US'
-            }
-        }
-    })
+  const [joke, loading, error, refetch] = useAxios({
+    axiosInstance: axios,
+    url: "/",
+    method: "GET",
+    requestConfig: {
+      headers: {
+        "Content-Language": "en-US",
+      },
+    },
+  });
 
   return (
     <article>
-        <h2>Random Dad Jokes</h2>
-        {loading && <p>Loading...</p>}
-        {!loading && error && <p className="errMsg">{error}</p> }
-        {!loading && !error && joke && <p>{joke?.joke}</p> }
-        {!loading && !error && !joke && <p>No Joke to Display! 😞</p> }
+      <h2>Random Dad Jokes</h2>
+      {loading && <p>Loading...</p>}
+      {!loading && error && <p className="errMsg">{error}</p>}
+      {!loading && !error && joke && <p>{joke?.joke}</p>}
+      {!loading && !error && !joke && <p>No Joke to Display! 😞</p>}
 
-        <button onClick={() => refetch()}>Get Joke</button>
+      <button onClick={() => refetch()}>Get Joke</button>
     </article>
-  )
-}
+  );
+};
 
-export default Jokes
+export default Jokes;
